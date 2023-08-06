e37a653b9c6a5edc3780ac73f9e3186b7cebc17c
Samer Saied <samerpasha@hotmail.com>
2023-08-06 06:49:21 +0300

    80% commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,21 @@
-
-import React from 'react';
-import NavBar from '../../components/widgets/navbar'
-import StartSection from '../../components/sections/startSection';
-import FooterSection from '../../components/sections/footerSection';
-import CardsWidget from '../../components/sections/cardsSection'
-import FaqWidget from '../../components/sections/faqSection'
-import  ContactMeWidget from '../../components/widgets/contactme';
+import React from "react";
+import NavBar from "../../components/widgets/navbar";
+import StartSection from "../../components/sections/startSection";
+import FooterSection from "../../components/sections/footerSection";
+import CardsWidget from "../../components/sections/cardsSection";
+import FaqWidget from "../../components/sections/faqSection";
+import ContactMeWidget from "../../components/widgets/contactme";
+import Head from "next/head";
 
 const { useState } = React;
 const App = () => {
-
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
   return (
     <>
+      <Head>
+        <title>Samer Saied</title>
+        <meta name="description" content="Samer Saied Resume" />
+      </Head>
       <NavBar></NavBar>
       <StartSection></StartSection>
       <CardsWidget />
