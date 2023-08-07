911852821c3e68e41e7b742465a819f0dc2d6451
Samer Saied <samerpasha@hotmail.com>
2023-08-07 07:30:19 +0300

    95% commit

--- src/pages/api/msg/index.js
+++ src/pages/api/msg/index.js
@@ -1,33 +1,23 @@
 
 import addMsg from "../../../../core/add"
 
-export default function handler(req, res) {
-  if (req.method === "POST") {
-    const data = req.body
-    return new Promise((resolve, reject) => {
-      addMsg(data.name, data.phone, data.body)
-        .then(response => {
-          res.statusCode = 200
-          res.setHeader('Content-Type', 'application/json');
-          res.setHeader('Cache-Control', 'max-age=180000');
-          return res.status(200).json({ result: response })
-          resolve();
-        })
-        .catch(error => {
-          res.json(error);
-          return res.status(405).end();
-          resolve(); // in case something goes wrong in the catch block (as vijay commented)
-        });
-    });
+const handler = async (req, res) => {
 
-
-
-
-  } else if (req.method === "GET") {
-    console.log("GET")
-    return res.status(200).json({ result: 'Test' })
-  } else {
-    return res.status(404).json({ result: 'Not Found' })
-
+  const data = req.body;
+  // res.header("Access-Control-Allow-Origin", "*");
+  switch (req.method) {
+    case "POST":
+      try {
+        await addMsg(data.name, data.phone, data.body)
+          .then(response => {
+            res.status(200).json({ result: response })
+          })
+      } catch (error) {
+        res.status(400).json({ result: error })
+      }
+      break
+    
   }
 }
+
+export default handler
