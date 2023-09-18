bfa9073fef121ac2deb2fab875e0eb386798ffda
Samer Saied <samerpasha@hotmail.com>
2023-09-18 11:47:33 +0300

    18-9-2023

--- src/pages/api/msg/index.js
+++ src/pages/api/msg/index.js
@@ -1,21 +1,21 @@
 
-import addMsg from "../../../../core/add"
+// import addMsg from "../../../../core/add"
 
-const handler = async (req, res) => {
+// const handler = async (req, res) => {
 
-  const data = req.body;
-  switch (req.method) {
-    case "POST":
-      try {
-        await addMsg(data.name, data.phone, data.body)
-          .then(response => {
-            res.status(200).json({ result: response })
-          })
-      } catch (error) {
-        res.status(400).json({ error: error })
-      }
-      break
-  }
-}
+//   const data = req.body;
+//   switch (req.method) {
+//     case "POST":
+//       try {
+//         await addMsg(data.name, data.phone, data.body)
+//           .then(response => {
+//             res.status(200).json({ result: response })
+//           })
+//       } catch (error) {
+//         res.status(400).json({ error: error })
+//       }
+//       break
+//   }
+// }
 
-export default handler
+// export default handler
